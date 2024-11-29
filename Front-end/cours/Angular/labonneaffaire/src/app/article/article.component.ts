5daf2bf603893a7626bfc6ac0ac131bca4128f8e
GUIGma W.Paulin <guigmawpaulin@gmail.com>
2024-11-29 19:13:11 +0100

    push du 29/11/2018

--- Front-end/cours/Angular/labonneaffaire/src/app/article/article.component.ts
+++ Front-end/cours/Angular/labonneaffaire/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit{
 
+  // String interpolation display
+  titreArticle: string = "Titre de l'article";
+  prixArticle: number = 12;
+
+  // Property biding on image "src"&"alt"
+  textAltImg: string = "Titre alternative de l'image";
+  urlImg: string = "https://via.placeholder.com/400x250";
+
+  // Event biding use
+  totalNbrLike: number = 0;
+
+  // two way biding
+  comment: string = "Ceci est une commentaire";
+  
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  onLike(){
+    this.totalNbrLike++;
+  }
 }
